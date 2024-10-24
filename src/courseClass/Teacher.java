57bb636cc081d991c7bd2ed1d99c71cdd1a3eea9
levenmartinov <EmailAdresiniz>
2024-10-24 23:25:34 +0100

    new version1

--- src/courseClass/Teacher.java
+++ src/courseClass/Teacher.java
@@ -2,26 +2,33 @@ package courseClass;
 
 public class Teacher extends Person{
 
-    String branch;
+    private String branch;
 
     public Teacher(String name, String branch) {
         super(name);
         this.branch = branch;
     }
 
+
     public String getBranch() {
         return branch;
     }
 
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+
     @Override
     public void printInfo() {
-        System.out.println("Teacher: " + this.name + ", Baranch: " + this.branch);
+        System.out.println("Teacher name : " + getName());
+        System.out.println("Branch : " + branch);
     }
 
     @Override
     public String toString() {
         return "Teacher{" +
-                "name='" + name + '\'' +
+                "name='" + getName() + '\'' +
                 ", branch='" + branch + '\'' +
                 '}';
     }
